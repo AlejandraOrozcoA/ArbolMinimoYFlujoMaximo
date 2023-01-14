999ae568b7fe2e9abaed338bddd3084f66a95f87
Juan Rene Hernandez Sanchez <jhernandezs1509@alumno.ipn.mx>
2023-01-13 19:00:29 -0600

    MST bien

--- js/ArbolMinimo.js
+++ js/ArbolMinimo.js
@@ -5,35 +5,35 @@ function elegirMetodo() {
     let select = document.getElementsByName("n1")[0];
     for (let i = select.options.length; i >= 1; i--) {
         select.options.remove(i);
-    }   
+    }
     select = document.getElementsByName("n2")[0];
     for (let i = select.options.length; i >= 1; i--) {
         select.options.remove(i);
-    }   
+    }
     select = document.getElementsByName("nInicial")[0];
     for (let i = select.options.length; i >= 1; i--) {
         select.options.remove(i);
     }
     switch (selector.value) {
-    case "1":
-        elemKruskal();
-        break;
-    case "2":
-        elemPrim();
-        break;
-    default:
-        document.getElementById("div-prim").style.display = "none";
-        document.getElementById("div-kruskal").style.display = "none";
-        break;
+        case "1":
+            elemKruskal();
+            break;
+        case "2":
+            elemPrim();
+            break;
+        default:
+            document.getElementById("div-prim").style.display = "none";
+            document.getElementById("div-kruskal").style.display = "none";
+            break;
     }
 }
 
 //Crea los elementos para Kruskal 
-function elemKruskal(){
+function elemKruskal() {
     document.getElementById("div-kruskal").style.display = "block";
     document.getElementById("div-prim").style.display = "none";
     let select = document.getElementsByName("nInicial")[0];
-    for(let i=0;  i < nodos.length; i++){
+    for (let i = 0; i < nodos.length; i++) {
         let option = document.createElement("option");
         option.text = nodos[i].text;
         select.add(option);
@@ -41,17 +41,17 @@ function elemKruskal(){
 }
 
 //Crea los elementos para Prim
-function elemPrim(){
+function elemPrim() {
     document.getElementById("div-kruskal").style.display = "none";
     document.getElementById("div-prim").style.display = "block";
     let select = document.getElementsByName("n1")[0];
-    for(let i=0;  i < nodos.length; i++){
+    for (let i = 0; i < nodos.length; i++) {
         let option = document.createElement("option");
         option.text = nodos[i].text;
         select.add(option);
     }
     select = document.getElementsByName("n2")[0];
-    for(let i=0;  i < nodos.length; i++){
+    for (let i = 0; i < nodos.length; i++) {
         let option = document.createElement("option");
         option.text = nodos[i].text;
         select.add(option);
@@ -59,202 +59,196 @@ function elemPrim(){
 }
 
 //Quita los elementos de los select al limpiar pantalla
-function removElementos(){
+function removElementos() {
     let select = document.getElementsByName("n1")[0];
     for (let i = select.options.length; i >= 1; i--) {
         select.options.remove(i);
-    }   
+    }
     select = document.getElementsByName("n2")[0];
     for (let i = select.options.length; i >= 1; i--) {
         select.options.remove(i);
-    }   
+    }
     select = document.getElementsByName("nInicial")[0];
     for (let i = select.options.length; i >= 1; i--) {
         select.options.remove(i);
-    }    
+    }
     selector.value = "0";
     document.getElementById("div-kruskal").style.display = "none";
     document.getElementById("div-prim").style.display = "none";
 }
 
-let nodoI;
-let val = true;
-let conActual;
-let conexionesActuales = [];
-let conexionesUsadas = [];
-let nodosVisitados = [];
-let sumaTotal = 0;
 
-function kruskal() {
-    if (val) {
-        let flag = validarKruskal();
-        val = false;
-        if (!flag) {
-            alert("Error de Validation");
-            return;
-        }
-    }
-    for (let i = 0; i < nodos.length; i++) {
-        if(nodos[i].text == nodoI || nodos[i].text == nodoI.text){
-            nodoI = nodos[i];
-            nodosVisitados.push(nodoI);
-            pintaRojo();
-        }
-    }
+//document.addEventListener('DOMContentLoaded',() => {
 
-    getConectados();
-    conexionesActuales.sort((a , b)=> (a.text - b.text) * (-1));
-
-    while (nodosVisitados.length < nodos.length) {
-        if (conexionesActuales.length <= 0){
-            alert("Kruskal Terminado")
-            return;
-        }
-
-        conActual = conexionesActuales.pop();
-        conexionesUsadas.push(conActual);
-
-        if (conActual == undefined){
-            alert("Error de Nodos");
-            return;
-        }
-        for (let nv in nodosVisitados) {
-            if (conActual.nodo1 == nv) {
-                nodoI = conActual.nodo2;
-            } else if(conActual.nodo2 == nv) {
-                nodoI = conActual.nodo1;
+    // Pinta un nodo que haya sido recorrido
+    function pintaRojo(nodo) {
+        for (let i = 0; i < nodos.length; i++) {
+            if(nodos[i] == nodo || nodos[i].text == nodo){
+                nodos[i].stroke_color = "red";
+                nodos[i].bg_color = "#CC0000";
+                reDraw();
             }
         }
+    }
 
-        if (!nodosVisitados.includes(nodoI)) {
-            let pos = conectores.indexOf(conActual);
-            let save = conectores[pos].text;
-            sumaTotal += parseInt(save);
-            conectores[pos].text = "("+ save + ")";
-            reDraw();
-            kruskal();
+    // Marca una conexion entre nodos que haya sido recorrido
+    function marcarConexion(a, b) {
+        for (let i = 0; i < conectores.length; i++) {
+            if (conectores[i].nodo1.text == a && conectores[i].nodo2.text == b) {
+                let save = conectores[i].text;
+                conectores[i].text = "(" + save + ")";
+                reDraw();
+                return true;
+            } else if (conectores[i].nodo1.text == b && conectores[i].nodo2.text == a) {
+                let save = conectores[i].text;
+                conectores[i].text = "(" + save + ")";
+                reDraw();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Valida los elementos seleccionados en la casilla
+    function validarKruskal() {
+        let select = document.getElementById("nInicial");
+        nodoI = select.options[select.selectedIndex].value;
+        if (nodoI === "....") {
+            alert("Selecciona un nodo de inicio");
+            return false;
+        }else{
+            let btn = document.getElementById("btnInicioKruskal");
+            btn.style.display = "block";
+            return true;
         }
     }
-}
 
-let nodo1;
-let nodo2;
-let let_MAX = Number.MAX_VALUE;
-function prim() {
-    validarPrim();
-    let inPrim = Array(nodos.length).fill(false);
-    let cost = Array(nodos.length).fill(Array(nodos.length));
-
-    for(let dist in conectores) {
-        let i = dist.nodo1.text.charCodeAt(0) - "A".charCodeAt(0);
-        let j = dist.nodo2.text.charCodeAt(0) - "A".charCodeAt(0);
-        cost[i][j] = dist.length;
-        cost[j][i] = dist.length;
+    // Valida los elementos seleccionados en la casilla
+    function validarPrim() {
+        let select1 = document.getElementById("n1");
+        nodo1 = select1.options[select1.selectedIndex].value;
+        let select2 = document.getElementById("n2");
+        nodo2 = select2.options[select2.selectedIndex].value;
+        if (nodo1 === "...." || nodo2 === "....") {
+            alert("Selecciona un nodo, no deje en Blanco");
+            return false;
+        } else if(!marcarConexion(nodo1,nodo2)){
+            alert("Selecciona un nodo valido");
+            return false;
+        } else{
+            let btn = document.getElementById("btnInicioPrim");
+            btn.style.display = "block";
+            return true;
+        }
     }
-    pintaRojo(nodo1);
-    pintaRojo(nodo2);
 
-    let edge_count = 0, mincost = 0;
-    while (edge_count < nodos.length - 1) {
-        let min = let_MAX, a = -1, b = -1;
-        for (let i = 0; i < nodos.length; i++) {
-            for (let j = 0; j < nodos.length; j++) {
-                if (cost[i][j] < min) {
-                    if (isValidEdge(i, j, inPrim)) {
-                        min = cost[i][j];
-                        a = i;
-                        b = j;
+    function generarGrafo(grafo) {
+        for (let i = 0; i < conectores.length; i++) {
+            let a = conectores[i].nodo1.text.charCodeAt(0) - "A".charCodeAt(0);
+            let b = conectores[i].nodo2.text.charCodeAt(0) - "A".charCodeAt(0);
+            grafo[a][b] = Number(conectores[i].text);
+            grafo[b][a] = Number(conectores[i].text);
+        }
+    }
 
+    function isValidEdge(u, v, inMST) {
+        if (u == v)
+            return false;
+        if (inMST[u] == false && inMST[v] == false)
+            return false;
+        else if (inMST[u] == true && inMST[v] == true)
+            return false;
+        return true;
+    }
+
+    function prim() {
+        let V = nodos.length;
+        let let_MAX = Number.MAX_VALUE
+        let inMST = Array(V).fill(false);
+        let grafo = Array(V).fill(Array(V).fill(let_MAX));
+        generarGrafo(grafo);
+
+        // Include first vertex in MST
+        inMST[0] = true;
+
+        // Keep adding edges while number of included
+        // edges does not become V-1.
+        let edge_count = 0, mincost = 0;
+        while (edge_count < V - 1) {
+
+            // Find minimum weight valid edge.
+            let min = let_MAX, a = -1, b = -1;
+            for (let i = 0; i < V; i++) {
+                for (let j = 0; j < V; j++) {
+                    if (grafo[i][j] < min) {
+                        if (isValidEdge(i, j, inMST)) {
+                            min = grafo[i][j];
+                            a = i;
+                            b = j;
+                        }
                     }
                 }
             }
-        }
 
-        if (a != -1 && b != -1) {
-
-            inPrim[b] = inPrim[a] = true;
-            let strA = String.fromCharCode(a + 65);
-            let strB = String.fromCharCode(b + 65);
-            pintaRojo(strA);
-            pintaRojo(strB);
-            marcarConexion(strA,strB);
-        }
-    }
-    alert("Costo minimo del MST = " + mincost);
-}
-
-function isValidEdge(u, v, inMST) {
-    if (u == v)
-        return false;
-    if (inMST[u] == false && inMST[v] == false)
-        return false;
-    else if (inMST[u] == true && inMST[v] == true)
-        return false;
-    return true;
-}
-
-function pintaRojo(nodo) {
-    for (let i = 0; i < nodos.length; i++) {
-        if(nodos[i] == nodo || nodos[i].text == nodo){
-            nodos[i].stroke_color = "red";
-            nodos[i].bg_color = "#CC0000";
-            reDraw();
-        }
-    }
-}
-
-function getConectados() {
-    for (let i = 0; i < conectores.length ; i++) {
-        if(conectores[i].nodo1 == nodoI || conectores[i].nodo2 == nodoI){
-            if (!conexionesActuales.includes(conectores[i])) {
-                if (!conexionesUsadas.includes(conectores[i])) {
-                    conexionesActuales.push(conectores[i]);
-                }
+            if (a != -1 && b != -1) {
+                edge_count++;
+                mincost = mincost + min;
+                inMST[b] = inMST[a] = true;
+                nodoAux = String.fromCharCode(a + 65); // 65 = "A"
+                pintaRojo(nodoAux);
+                nodoAux = String.fromCharCode(b + 65); // 65 = "A"
+                pintaRojo(nodoAux);
             }
         }
+        alert("Minimum cost = " + mincost);
     }
-}
 
-function validarKruskal() {
-    let select = document.getElementById('nInicial');
-    nodoI = select.options[select.selectedIndex].value;
-    if (nodoI === "....") {
-        alert("Selecciona un nodo de inicio");
-        return false;
-    }else{
-        let btn = document.getElementById("btnInicioKruskal");
-        btn.style.display = "block";
-        return true;
+    var parent = Array(nodos.length).fill(0);
+
+    function find(i) {
+        while (parent[i] != i)
+            i = parent[i];
+        return i;
     }
-}
 
-function validarPrim() {
-    let select1 = document.getElementById('n1');
-    nodo1 = select1.options[select1.selectedIndex].value;
-    let select2 = document.getElementById('n2');
-    nodo2 = select2.options[select2.selectedIndex].value;
-    if (nodo1 === "...." || nodo2 === "....") {
-        alert("Selecciona un nodo valido");
-        return false;
-    }else{
-        let btn = document.getElementById("btnInicioPrim");
-        btn.style.display = "block";
-        return true;
+    function union1(i, j) {
+        let a = find(i);
+        let b = find(j);
+        parent[a] = b;
     }
-}
 
-function marcarConexion(a, b){
-    for (let i = 0; i < conectores.length; i++) {
-        if(conectores[i].nodo1.text == a && conectores[i].nodo1.text == b){
-            let save = conectores[i].text;
-            sumaTotal += parseInt(save);
-            conectores[i].text = "("+ save + ")";
-            reDraw();
-        }else if(conectores[i].nodo1.text == b && conectores[i].nodo1.text == a){
-            let save = conectores[i].text;
-            sumaTotal += parseInt(save);
-            conectores[i].text = "("+ save + ")";
-            reDraw();
+    function kruskal() {
+        let mincost = 0; // Cost of min MST.
+        let V = nodos.length;
+        let let_MAX = Number.MAX_VALUE;
+        let grafo = Array(V).fill(Array(V).fill(let_MAX));
+        generarGrafo(grafo);
+
+        for (let i = 0; i < V; i++)
+            parent[i] = i;
+
+        let edge_count = 0;
+        while (edge_count < V - 1) {
+            let min = let_MAX, a = -1, b = -1;
+            for (let i = 0; i < V; i++) {
+                for (let j = 0; j < V; j++) {
+                    if (find(i) != find(j) && grafo[i][j] < min) {
+                        min = grafo[i][j];
+                        a = i;
+                        b = j;
+                    }
+                }
+            }
+
+            union1(a, b);
+            edge_count++;
+            mincost += min;
+            nodoAux = String.fromCharCode(a + 65); // 65 = "A"
+            pintaRojo(nodoAux);
+            nodoAux = String.fromCharCode(b + 65); // 65 = "A"
+            pintaRojo(nodoAux);
+            
         }
+        alert("Minimum cost = " + mincost);
     }
-}
+//})
