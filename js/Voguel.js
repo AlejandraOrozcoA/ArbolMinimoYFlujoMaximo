b133f9e0e27a15489acafeb257edbea4b342ce29
Alejandra Orozco Aguilar <ale12orozco@gmail.com>
2023-01-15 19:17:05 -0600

    encuantra mayor penalizacion

--- js/Voguel.js
+++ js/Voguel.js
@@ -1,5 +1,9 @@
 let filP = [];
+let filPMax = 0;
 let colP = [];
+let colPMax = 0;
+
+let penalizacion;
 let num1 = Number.MAX_SAFE_INTEGER;
 let num2 = Number.MAX_SAFE_INTEGER;
 
@@ -38,5 +42,33 @@ function  calcularPenalizacion() {
     colP.push(penalizacion);
     num1 = Number.MAX_SAFE_INTEGER;
     num2 = Number.MAX_SAFE_INTEGER;
+  }
+}
+
+function Penalizacion(valor, index, pos){
+  this.valor =  valor;
+  this.index = index;
+  this.posicion = pos;
+}
+
+function mayorPenalizacion() {
+  let indexI = 0;
+  let indexJ = 0;
+  for (let i = 0; i < filP.length; i++) {
+    if (filP[i] > filPMax) {
+      filPMax = filP[i];
+      indexI = i;
+    }
+  }
+  for (let j = 0; j < colP.length; j++) {
+    if (colP[j] > colPMax) {
+      colPMax = colP[j];
+      indexJ = j;
+    }
+  }
+  if (colPMax > filPMax) {
+    penalizacion = new Penalizacion(colPMax,indexJ,'columna');
+  } else {
+    penalizacion = new Penalizacion(filPMax,indexI,'fila');
   }
 }
