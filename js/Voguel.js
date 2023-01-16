226e3020991f3446abfaa79ba77805cd64d65cba
Alejandra Orozco Aguilar <ale12orozco@gmail.com>
2023-01-15 21:05:03 -0600

    correccion al calcular penalizaciones

--- js/Voguel.js
+++ js/Voguel.js
@@ -3,7 +3,7 @@ let filPMax = 0;
 let colP = [];
 let colPMax = 0;
 
-let penalizacion;
+let penalizacion = new Celda ( Number.MAX_SAFE_INTEGER, null, null, null, null);
 let num1 = Number.MAX_SAFE_INTEGER;
 let num2 = Number.MAX_SAFE_INTEGER;
 
@@ -13,6 +13,7 @@ function  calcularPenalizacion() {
   //Penalizacion por fila 
   for (let i = 0; i < numFuentes; i++) {
     for (let j = 0; j < numDestinos; j++) {
+      if(esSatisfecha(filSatisfecha,i+1) == false && esSatisfecha(colSatisfecha,j+1) == false){
         if (num2 > celdas[i][j].costo) {
           num2 = celdas[i][j].costo;
           if (num1 > num2) {
@@ -21,9 +22,12 @@ function  calcularPenalizacion() {
             num2 = aux;
           }
         }
+      }
     }
-    let penalizacion = num2-num1;
-    filP.push(penalizacion); 
+    if(esSatisfecha(filSatisfecha,i+1) == false){
+      let penalizacion = num2-num1;
+      filP.push(penalizacion); 
+    }
     num1 = Number.MAX_SAFE_INTEGER;
     num2 = Number.MAX_SAFE_INTEGER;
   }
@@ -31,17 +35,21 @@ function  calcularPenalizacion() {
   //Penalizaion por columna 
   for (let j = 0; j < numDestinos; j++) {
     for (let i = 0; i < numFuentes; i++) {
-      if (num2 > celdas[i][j].costo) {
-          num2 = celdas[i][j].costo;
-          if (num1 > num2) {
-            let aux = num1;
-            num1 = num2;
-            num2 = aux;
-          }
+      if(esSatisfecha(filSatisfecha,i+1) == false && esSatisfecha(colSatisfecha,j+1) == false){
+        if (num2 > celdas[i][j].costo) {
+            num2 = celdas[i][j].costo;
+            if (num1 > num2) {
+              let aux = num1;
+              num1 = num2;
+              num2 = aux;
+            }
+        }
       }
     }
-    let penalizacion = num2-num1;
-    colP.push(penalizacion);
+    if(esSatisfecha(colSatisfecha,j+1) == false){
+      let penalizacion = num2-num1;
+      colP.push(penalizacion);
+    }
     num1 = Number.MAX_SAFE_INTEGER;
     num2 = Number.MAX_SAFE_INTEGER;
   }
@@ -58,15 +66,19 @@ function mayorPenalizacion() {
   let indexI = 0;
   let indexJ = 0;
   for (let i = 0; i < filP.length; i++) {
-    if (filP[i] > filPMax) {
-      filPMax = filP[i];
-      indexI = i;
+    if (esSatisfecha(filSatisfecha,i+1) == false) {
+      if (filP[i] > filPMax) {
+        filPMax = filP[i];
+        indexI = i;
+      }
     }
   }
   for (let j = 0; j < colP.length; j++) {
-    if (colP[j] > colPMax) {
-      colPMax = colP[j];
-      indexJ = j;
+    if (esSatisfecha(colSatisfecha,j+1)==false) {
+      if (colP[j] > colPMax) {
+        colPMax = colP[j];
+        indexJ = j;
+      }
     }
   }
   if (colPMax > filPMax) {
@@ -83,21 +95,21 @@ function encontrarMenor() {
 
   if (penalizacion.posicion == 'fila') {
     for (let i = 0; i < numDestinos ; i++) {
-      if(esSatisfecha(filSatisfecha,index) == false && esSatisfecha(colSatisfecha,i) == false){
+      //if(esSatisfecha(filSatisfecha,index) == false && esSatisfecha(colSatisfecha,i+1) == false){
         if (menor > celdas[index][i].costo) {
           menor = celdas[index][i].costo;
           celdaMinimo = celdas[index][i];
         }
-      }
+      //}
     }
   } else {
     for (let i = 0; i < numFuentes; i++) {
-      if(esSatisfecha(filSatisfecha,i) == false && esSatisfecha(colSatisfecha,index) == false){
+      //if(esSatisfecha(filSatisfecha,i+1) == false && esSatisfecha(colSatisfecha,index) == false){
         if (menor > celdas[i][index].costo) {
           menor = celdas[i][index].costo;
           celdaMinimo = celdas[index][i];
         }
-      }
+      //}
     }
   }
   revisarOfertaDemanda();
